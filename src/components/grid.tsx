178932e817ebc3e0ae0ea419dc6a1c4dc6e81753
liuzewei <liuzewei@outlook.com>
2025-03-11 10:08:41 +0800

    improve grid component

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Row, RowProps } from 'antd';
 import { Space } from '../common';
 
-export const Grid = (props: RowProps) => {
+export const Grid = React.forwardRef(function Grid(
+  props: RowProps,
+  ref: React.ForwardedRef<HTMLDivElement>
+) {
   const { gutter = [Space, Space], ...rest } = props;
-  return <Row {...rest} gutter={gutter} />;
-};
+  return <Row {...rest} gutter={gutter} ref={ref} />;
+});
