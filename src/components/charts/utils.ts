ada0d4f96d4b5daebc145de465f1b7d6f6c8b520
liuzewei <liuzewei@outlook.com>
2025-09-15 15:37:15 +0800

    fix global variable in function's scope

--- src/components/charts/utils.ts
+++ src/components/charts/utils.ts
@@ -17,7 +17,7 @@ export const chartColors = [
 ];
 
 export function getOptions(...opts: ECOptions[]) {
-  const color = chartColors;
+  const color = [...chartColors];
   const grid = {
     top: '12%',
     bottom: '10%',
